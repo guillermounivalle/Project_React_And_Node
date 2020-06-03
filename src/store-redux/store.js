0f564e3f5b8172bdde8fee8b2df60339335dc46a
guillermounivalle <guillermo.hernandez@correounivalle.edu.co>
2020-06-03 14:26:26 -0500

    making branch develop

---
+++ src/store-redux/store.js
@@ -0,0 +1,16 @@
+import {createStore} from "redux";
+
+/**Estado inicial se indica cual es para que cuando ncargue la pàgina por primera vez
+ * no cargue un estado "undefined" y genere error
+ */
+const initialState = {
+    players: [],
+    titular_players: [],
+    substitute_players: []
+}
+
+const reducerCoach =(state = initialState, action) => {
+    return state;
+}
+
+export default createStore(reducerCoach);
