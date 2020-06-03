b0f63a48f2d9cfef7a4215ae2f771630cd86b2e2
guillermounivalle <guillermo.hernandez@correounivalle.edu.co>
2020-06-03 11:00:25 -0500

    making structure basic project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+
+/**
+ * Componente provider es de la libreria react-redux. Debe envolver toda la aplicación.
+ */
+import {provider} from "react-redux"
+
+//Importamos el store
+import store from "./store-redux/store"
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store = {store}>
+        <main>
+         <h1>ED manager</h1>
+         <Players/>
+         <SelectedTeam/>
+       </main>
+      </Provider>
     );
   }
 }
